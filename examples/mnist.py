ead3e4600155276425164700b12b51e14c101b5d
c1aris <goolhanrry@gmail.com>
2020-03-18 23:37:05 +0800

    Add examples into CI

--- examples/mnist.py
+++ examples/mnist.py
@@ -1,8 +1,9 @@
 # Copyright (c) Ye Liu. All rights reserved.
 
-import torch
-import torchvision
 import torch.nn as nn
+import torchvision
+from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST
 
 import nncore
 from nncore.engine import Engine
@@ -41,26 +42,25 @@ class LeNet(nn.Module):
 
 
 def main():
+    # Load config from files
     cfg = nncore.Config.from_file('examples/config.py')
 
+    # Prepare the dataset and model
     transform = torchvision.transforms.Compose([
         torchvision.transforms.ToTensor(),
         torchvision.transforms.Normalize([0.5], [0.5])
     ])
 
-    trainset = torchvision.datasets.MNIST(
-        'data', train=True, transform=transform, download=True)
-    trainloader = torch.utils.data.DataLoader(
-        trainset, batch_size=16, shuffle=True, num_workers=2)
+    train = MNIST('data', train=True, transform=transform, download=True)
+    trainloader = DataLoader(train, batch_size=16, shuffle=True, num_workers=2)
 
-    testset = torchvision.datasets.MNIST(
-        'data', train=False, transform=transform, download=True)
-    testloader = torch.utils.data.DataLoader(
-        testset, batch_size=16, shuffle=False, num_workers=2)
+    test = MNIST('data', train=False, transform=transform, download=True)
+    testloader = DataLoader(test, batch_size=16, shuffle=False, num_workers=2)
 
     data_loaders = dict(train=trainloader, val=testloader)
     model = LeNet()
 
+    # Initialize and launch engine
     engine = Engine(model, data_loaders, **cfg)
     engine.launch()
 
