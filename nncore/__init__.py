fbb2387c2b25ed2af87860b66d5c818de2051a19
c1aris <goolhanrry@gmail.com>
2020-03-25 19:29:49 +0800

    Bump version to 0.0.1

--- nncore/__init__.py
+++ nncore/__init__.py
@@ -5,6 +5,6 @@ import sentry_sdk
 from .fileio import *  # noqa: F401,F403
 from .utils import *  # noqa: F401,F403
 
-__version__ = '0.0.1rc2'
+__version__ = '0.0.1'
 
 sentry_sdk.init('https://59e084fdf48d40a995fd8d85621b9f58@sentry.io/4107977')
