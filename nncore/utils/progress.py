3065ed14035b7188328d060d5e80ed8795f7875f
Ye Liu <yeliudev@outlook.com>
2022-05-06 11:25:51 +0800

    Update progress bar

--- nncore/utils/progress.py
+++ nncore/utils/progress.py
@@ -12,8 +12,8 @@ class ProgressBar(object):
     effect in the main process.
     """
 
-    _wb = '\r[{{}}] {}/{}, {:.1f} task/s, elapsed: {}s, eta: {}s{}'
-    _ob = '\rcompleted: {}, elapsed: {}s, {:.1f} tasks/s'
+    _wb = '\r[{{}}] {}/{}, {:.1f} task/s, elapsed: {}, eta: {}{}'
+    _ob = '\rcompleted: {}, elapsed: {}, {:.1f} tasks/s'
 
     def __init__(self, num_tasks=None, active=None):
         self._task_num = num_tasks
@@ -45,6 +45,36 @@ class ProgressBar(object):
         bar_width = min(int(width - len(msg)) + 2, int(width * 0.6), 40)
         return max(2, bar_width)
 
+    def _get_time_str(self, second):
+        if second >= 86400:
+            day = int(second / 86400)
+            second -= (day * 86400)
+            day = '{}d'.format(day)
+        else:
+            day = ''
+
+        if second >= 3600:
+            hour = int(second / 3600)
+            second -= (hour * 3600)
+            hour = '{}h'.format(hour)
+        else:
+            hour = ''
+
+        if second >= 60:
+            minute = int(second / 60)
+            second -= minute * 60
+            minute = '{}m'.format(minute)
+        else:
+            minute = ''
+
+        if second > 0:
+            second = '{}s'.format(second)
+        else:
+            second = ''
+
+        time_str = '{}{}{}{}'.format(day, hour, minute, second) or '0s'
+        return time_str
+
     def update(self, times=1):
         if not self._active:
             return
@@ -53,19 +83,21 @@ class ProgressBar(object):
             self._completed += 1
             ela = self._timer.seconds()
             fps = self._completed / ela
+            ela_str = self._get_time_str(ceil(ela))
 
             if self._task_num is not None:
                 perc = self._completed / float(self._task_num)
+                eta = int(ela * (1 - perc) / perc + 0.5)
+                eta_str = self._get_time_str(ceil(eta))
                 msg = self._wb.format(
-                    self._completed, self._task_num, fps, ceil(ela),
-                    int(ela * (1 - perc) / perc + 0.5),
+                    self._completed, self._task_num, fps, ela_str, eta_str,
                     '\n' if self._task_num == self._completed else '')
                 bar_width = self._get_bar_width(msg)
                 mark_width = int(bar_width * perc)
                 chars = '>' * mark_width + ' ' * (bar_width - mark_width)
                 msg = msg.format(chars)
             else:
-                msg = self._ob.format(self._completed, ceil(ela), fps)
+                msg = self._ob.format(self._completed, ela_str, fps)
 
             print(msg.ljust(self._last_length), end='')
             self._last_length = len(msg)
