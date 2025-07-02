b295301f86dfe7523af2a79e1c7ceb9c45517927
Ye Liu <yeliudev@outlook.com>
2025-07-02 18:46:14 +0800

    Fix cwd command

--- nncore/utils/__init__.py
+++ nncore/utils/__init__.py
@@ -10,7 +10,7 @@ from .env import (collect_env_info, exec, get_host_info, get_time_str,
 from .logging import get_logger, log, set_default_logger
 from .misc import recursive
 from .network import download
-from .path import (abs_path, base_name, cp, dir_name, expand_user, find,
+from .path import (abs_path, base_name, cp, cwd, dir_name, expand_user, find,
                    get_size, is_dir, is_file, join, ls, mkdir, mv, pure_ext,
                    pure_name, rel_path, remove, rename, same_dir, split_ext,
                    split_path, symlink)
@@ -24,8 +24,8 @@ __all__ = [
     'swap_element', 'to_dict_of_list', 'to_list_of_dict', 'collect_env_info',
     'exec', 'get_host_info', 'get_time_str', 'get_timestamp', 'get_logger',
     'log', 'set_default_logger', 'recursive', 'download', 'abs_path',
-    'base_name', 'cp', 'dir_name', 'expand_user', 'find', 'get_size', 'is_dir',
-    'is_file', 'join', 'ls', 'mkdir', 'mv', 'pure_ext', 'pure_name',
+    'base_name', 'cp', 'cwd', 'dir_name', 'expand_user', 'find', 'get_size',
+    'is_dir', 'is_file', 'join', 'ls', 'mkdir', 'mv', 'pure_ext', 'pure_name',
     'rel_path', 'remove', 'rename', 'same_dir', 'split_ext', 'split_path',
     'symlink', 'ProgressBar', 'Registry', 'build_object', 'Timer'
 ]
